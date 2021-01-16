04503582f7e78fa830443d53b03e3980aba59e00
Victor Cercasin <victorhugo.cercasin@gmail.com>
2021-01-16 18:24:18 -0300

    Ajustes na userDrawer

--- src/components/PageHeader/UserIcon/DrawerLinks.js
+++ src/components/PageHeader/UserIcon/DrawerLinks.js
@@ -9,13 +9,15 @@ import "./DrawerLinks.css";
 
 const DrawerLinks = (props) => {
   return (
-    <ul id='drawer-links'>
-      <li><Button to='/meus-cursos'>Meus Cursos</Button></li>
-      <li><Button to='/minha-conta'>Minha conta</Button></li>
-      <li><Button to='/carrinho'>Carrinho</Button></li>
-      <li><Button to='/area-do-administrador'>Area do Administrador</Button></li>
+    <div className="drawer-links">
+      <ul>
+        <li><Button to='/meus-cursos' id='button'>Meus Cursos</Button></li>
+        <li><Button to='/minha-conta'id='button'>Minha conta</Button></li>
+        <li><Button to='/carrinho'id='button'>Carrinho</Button></li>
+        <li><Button to='/area-do-administrador'id='button'>Area do Administrador</Button></li>
 
-    </ul>
+      </ul>
+    </div>
   );
 };
 
--- src/pages/Inicial/Inicial.js
+++ src/pages/Inicial/Inicial.js
@@ -6,17 +6,12 @@ import "./Inicial.css"
 const  Inicial = () => {
     return (
         <div id="page-inicial" className='page'>
-            <div id="page-inicial-content">
-                <div className="nome-container">
-                    <h1>SEJA UM POLICIAL</h1>
-                    <h2><i>Plataforma de estudos online focado na área policial.</i></h2>
-                </div>
-
-
+            <div className="nome-container">
+                <h1>SEJA UM POLICIAL</h1>
+                <h2><i>Plataforma de estudos online focado na área policial.</i></h2>
             </div>
-
         </div>
-    )
+    ) 
 }
 
 export default Inicial;
