b52a8487b911cb74069f7e58b2d34cdfa1194c69
Enzo <enzoguido.a375@uel.br>
2021-04-16 21:09:57 -0300

    task-caderno-gratuito

---
+++ src/pages/CadernoGratuito/CadernoGratuito.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+import "./CadernoGratuito.css";
+
+const  CadernoGratuito = () => {
+    return (
+        <div id="caderno-gratuito" className='page' >
+            <div className="titulo">
+                <h1>QUESTOES GRATUITAS</h1>
+            </div>
+            <div className="lista">
+                <ul>
+                    <li>
+                        <p>+ 100 QUESTÕES</p>
+                    </li>
+                    <li>
+                        <p>MATERIAL ATUALIZADO</p>
+                    </li>
+                    <li>
+                        <p>DOWNLOAD GRÁTIS</p>
+                    </li>
+                    <li>
+                        <p>MATERIAL DIGITAL</p>
+                    </li>
+                    <li>
+                        <p>QUESTÕES GABARITADAS</p>
+                    </li>
+                    <li>
+                        <p>QUESTÕES POLICIAIS</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    ) 
+}
+
+export default CadernoGratuito;
